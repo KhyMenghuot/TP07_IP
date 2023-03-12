1c9800b3f7a6de6b30ddd5d2b305ce0e65616893
unknown <khymenghuot@gmail.com>
2023-03-13 00:45:51 +0700

    upload file

---
+++ Exercise3/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.get('/', function(req, res) {
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.send("There is a server error");
+
+        }
+        res.send(data);
+    });
+
+})
+app.get('/detail', function(req, res) {
+    fs.readFile('detail.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.send("There is a server error");
+
+        }
+        res.send(data);
+    });
+
+})
+const port = 3000
+app.listen(port, () => {
+    console.log("Running localhost:" + port)
+})
